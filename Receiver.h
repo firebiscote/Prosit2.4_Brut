f1b86b32ce59a841c25e97d5822efb28e6e95e72
Maxime CAVARROC <maxime.cavarroc@viacesi.fr>
2020-11-16 15:06:27 +0100

    deb

--- Receiver.h
+++ Receiver.h
@@ -1,8 +1,10 @@
 #pragma once
 
-
+#include <iostream>
 
 class Receiver {
+public:
+
+private:
 
 };
-
--- Transmitter.h
+++ Transmitter.h
@@ -1,8 +1,10 @@
 #pragma once
 
-
+#include <iostream>
 
 class Transmitter {
+public:
+
+private:
 
 };
-
